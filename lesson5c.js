d8979d8806d0f53415158cb07fa184d410d685b1
MODCOM INSTITUTE <isaacm2540@gmail.com  git config --global user.name Elprezidanteecho>
2026-03-04 11:42:24 +0300

    updated

---
+++ lesson5c.js
@@ -0,0 +1,13 @@
+// Exporting modules
+// We export modules so that we can use/utilise them in other files/
+// modules in our program
+
+export const calculator = (x, y) => {
+    let difference = x - y
+
+    console.log("The difference of the two numbers is: ", difference)
+}
+
+
+export const simpleInterest =(principle,rate,time)
+console.log(findVolume(5, 3, 2));
